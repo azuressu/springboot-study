09c227d06aa99481aba2f7e93e5c8b4c408d84c2
azuressu <martina0130@naver.com>
2025-01-22 22:21:37 +0900

    IoC, DI

--- src/main/java/com/memo/controller/MemoController.java
+++ src/main/java/com/memo/controller/MemoController.java
@@ -3,7 +3,6 @@ package com.memo.controller;
 import com.memo.dto.MemoRequestDto;
 import com.memo.dto.MemoResponseDto;
 import com.memo.service.MemoService;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,8 +13,8 @@ public class MemoController {
 
     private final MemoService memoService;
 
-    public MemoController(JdbcTemplate jdbcTemplate) {
-        this.memoService = new MemoService(jdbcTemplate);
+    public MemoController(MemoService memoService) {
+        this.memoService = memoService;
     }
 
     @PostMapping("/memos")
--- src/main/java/com/memo/service/MemoService.java
+++ src/main/java/com/memo/service/MemoService.java
@@ -4,7 +4,6 @@ import com.memo.dto.MemoRequestDto;
 import com.memo.dto.MemoResponseDto;
 import com.memo.entity.Memo;
 import com.memo.repository.MemoRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 
@@ -12,8 +11,8 @@ public class MemoService {
 
     private final MemoRepository memoRepository;
 
-    public MemoService(JdbcTemplate jdbcTemplate) {
-        this.memoRepository = new MemoRepository(jdbcTemplate);
+    public MemoService(MemoRepository memoRepository) {
+        this.memoRepository = memoRepository;
     }
 
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
