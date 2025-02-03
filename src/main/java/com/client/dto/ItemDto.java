e29e10a451f61b8e6485d779876e6398f342703c
azuressu <martina0130@naver.com>
2025-02-03 15:27:54 +0900

    init commit

---
+++ src/main/java/com/client/dto/ItemDto.java
@@ -0,0 +1,11 @@
+package com.client.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ItemDto {
+    private String title;
+    private int price;
+}
